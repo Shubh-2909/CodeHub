ee5236e755c4a3624afb86d0caa14728873e5c4b
Shubh-2909 <shubhshubhanjal96@gmail.com>
2024-11-07 10:59:34 +0530

    login&signup

---
+++ src/components/Modals/Signup.tsx
@@ -0,0 +1,81 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+
+type SignupProps = {};
+
+const Signup: React.FC<SignupProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const handleClick = (type: "login" | "register" | "forgotPassword") => {
+    setAuthModalState((prev) => ({ ...prev, type }));
+  };
+  return (
+    <form className="space-y-6 px-6 pb-4">
+      <h3 className="text-xl font-medium text-white">Register to Codehub</h3>
+      <div>
+        <label
+          htmlFor="email"
+          className="text-sm font-medium block mb-2 text-gray-300"
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="name@company.com"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="name"
+          className="text-sm font-medium block mb-2 text-gray-300"
+        >
+          Your Name
+        </label>
+        <input
+          type="name"
+          name="name"
+          id="name"
+          placeholder="Shubh Shubhanjal"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="text-sm font-medium block mb-2 text-gray-300"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="********"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 text-center bg-brand-orange hover:bg-brand-orange-s"
+      >
+        Register
+      </button>
+
+      <div className="text-sm font-medium text-gray-300">
+        Already have an account?{" "}
+        <a
+          href="#"
+          className="text-blue-700 hover:underline"
+          onClick={() => handleClick("login")}
+        >
+          Log In
+        </a>
+      </div>
+    </form>
+  );
+};
+export default Signup;
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8-YkzUWPDU4dg30QTfwoRuG1tJUM3LgI",
+  authDomain: "codehub-79571.firebaseapp.com",
+  projectId: "codehub-79571",
+  storageBucket: "codehub-79571.firebasestorage.app",
+  messagingSenderId: "929748743398",
+  appId: "1:929748743398:web:9f5c31e260dd3c74543727",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
